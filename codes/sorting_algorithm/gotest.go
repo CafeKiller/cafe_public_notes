5133a42446084132fe6cdc180db85d1e22f0ca65
liuxinzan <18320210361@163.com>
2024-05-07 12:01:18 +0800

    feat: 添加希尔、快速、归并排序

--- codes/sorting_algorithm/gotest.go
+++ codes/sorting_algorithm/gotest.go
@@ -49,6 +49,92 @@ func insertionSort(arr []int) []int {
     return arr
 }
 
+
+// shellSort œ£∂˚≈≈–Ú
+func shellSort(arr []int) []int {
+    length := len(arr)
+    gap := 1
+    for gap < gap/3 {
+        gap = gap*3 + 1
+    }
+    for gap > 0 {
+        for i := gap; i < length; i++ {
+            temp := arr[i]
+            j := i - gap
+            for j >= 0 && arr[j] > temp {
+                arr[j+gap] = arr[j]
+                j -= gap
+            }
+            arr[j+gap] = temp
+        }
+        gap = gap / 3
+    }
+    return arr
+}
+
+// mergeSort πÈ≤¢≈≈–Ú
+func mergeSort(arr []int) []int {
+    length := len(arr)
+    if length < 2 {
+        return arr
+    }
+    middle := length / 2
+    left := arr[0:middle]
+    right := arr[middle:]
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+// merge πÈ≤¢µ›πÈ¥¶¿Ì∫Ø ˝
+func merge(left []int, right []int) []int {
+    var result []int
+    for len(left) != 0 && len(right) != 0 {
+        if left[0] <= right[0] {
+            result = append(result, left[0])
+            left = left[1:]
+        } else {
+            result = append(result, right[0])
+            right = right[1:]
+        }
+    }
+    for len(left) != 0 {
+        result = append(result, left[0])
+        left = left[1:]
+    }
+    for len(right) != 0 {
+        result = append(result, right[0])
+        right = right[1:]
+    }
+    return result
+}
+
+// quickSort øÏÀŸ≈≈–Ú
+func quickSort(arr []int) []int {
+    return _quickSort(arr, 0, len(arr)-1)
+}
+func _quickSort(arr []int, left, right int) []int {
+    if left < right {
+        partitionIndex := partition(arr, left, right)
+        _quickSort(arr, left, partitionIndex-1)
+        _quickSort(arr, partitionIndex+1, right)
+    }
+    return arr
+}
+func partition(arr []int, left, right int) int {
+    pivot := left
+    index := pivot + 1
+    for i := index; i <= right; i++ {
+        if arr[i] < arr[pivot] {
+            swap(arr, i, index)
+            index += 1
+        }
+    }
+    swap(arr, pivot, index-1)
+    return index - 1
+}
+func swap(arr []int, i, j int) {
+    arr[i], arr[j] = arr[j], arr[i]
+}
+
 func main() {
 
 }
--- codes/sorting_algorithm/pytest.py
+++ codes/sorting_algorithm/pytest.py
@@ -33,6 +33,74 @@ def insertionSort(arr):
     return arr
 
 
+# 希尔排序
+def shellSort(arr):
+    import math
+    gap=1
+    while(gap < len(arr)/3):
+        gap = gap*3+1
+    while gap > 0:
+        for i in range(gap,len(arr)):
+            temp = arr[i]
+            j = i-gap
+            while j >=0 and arr[j] > temp:
+                arr[j+gap]=arr[j]
+                j-=gap
+            arr[j+gap] = temp
+        gap = math.floor(gap/3)
+    return arr
+
+
+# 归并排序
+def mergeSort(arr):
+    import math
+    if(len(arr)<2):
+        return arr
+    middle = math.floor(len(arr)/2)
+    left, right = arr[0:middle], arr[middle:]
+    return merge(mergeSort(left), mergeSort(right))
+
+# 归并递归处理函数
+def merge(left,right):
+    result = []
+    while left and right:
+        if left[0] <= right[0]:
+            result.append(left.pop(0));
+        else:
+            result.append(right.pop(0));
+    while left:
+        result.append(left.pop(0));
+    while right:
+        result.append(right.pop(0));
+    return result
+
+# 快速排序
+def quickSort(arr, left=None, right=None):
+    left = 0 if not isinstance(left,(int, float)) else left
+    right = len(arr)-1 if not isinstance(right,(int, float)) else right
+    if left < right:
+        partitionIndex = partition(arr, left, right)
+        quickSort(arr, left, partitionIndex-1)
+        quickSort(arr, partitionIndex+1, right)
+    return arr
+
+# 快速排序处理基准
+def partition(arr, left, right):
+    pivot = left
+    index = pivot+1
+    i = index
+    while  i <= right:
+        if arr[i] < arr[pivot]:
+            swap(arr, i, index)
+            index+=1
+        i+=1
+    swap(arr,pivot,index-1)
+    return index-1
+
+# 快速排序交换
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
 
 if __name__ == '__main__':
     print('test start')
