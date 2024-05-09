b0f35bb39fcd0e14a1acbea7fe8d1aa142f8e67e
CoffeeKiller <18320210361@163.com>
2024-05-09 23:08:26 +0800

    test: 添加 python 版本的测试代码

--- codes/sorting_algorithm/pytest.py
+++ codes/sorting_algorithm/pytest.py
@@ -1,8 +1,32 @@
+TOTAL = 5000
+
+
+# 测试函数
+def sortTest(func, total=1000):
+    import random, copy, operator, math, time
+
+    arrList = [i for i in range(-math.floor(total / 2), math.ceil(total / 2))]
+    arrListR = copy.deepcopy(arrList)
+    while operator.eq(arrList, arrListR):
+        random.shuffle(arrListR)
+    start = time.clock()
+    arrListR = func(arrListR)
+    end = time.clock()
+    runtime = end - start
+    if operator.eq(arrList, arrListR):
+        print("[Success]", func.__name__, "with Total:", len(arrList), "in %.5fs" % runtime)
+        return True
+    else:
+        print("[Fail]", func.__name__, "with Total:", len(arrList), "in %.5fs" % runtime)
+        return False
+    pass
+
+
 # 冒泡排序
 def bubbleSort(arr):
     for i in range(1, len(arr)):
-        for j in range(0, len(arr)-i):
-            if arr[j] > arr[j+1]:
+        for j in range(0, len(arr) - i):
+            if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
@@ -24,44 +48,45 @@ def selectionSort(arr):
 # 插入排序
 def insertionSort(arr):
     for i in range(len(arr)):
-        preIndex = i-1
+        preIndex = i - 1
         current = arr[i]
         while preIndex >= 0 and arr[preIndex] > current:
-            arr[preIndex+1] = arr[preIndex]
-            preIndex-=1
-        arr[preIndex+1] = current
+            arr[preIndex + 1] = arr[preIndex]
+            preIndex -= 1
+        arr[preIndex + 1] = current
     return arr
 
 
 # 希尔排序
 def shellSort(arr):
     import math
-    gap=1
-    while(gap < len(arr)/3):
-        gap = gap*3+1
+    gap = 1
+    while (gap < len(arr) / 3):
+        gap = gap * 3 + 1
     while gap > 0:
-        for i in range(gap,len(arr)):
+        for i in range(gap, len(arr)):
             temp = arr[i]
-            j = i-gap
-            while j >=0 and arr[j] > temp:
-                arr[j+gap]=arr[j]
-                j-=gap
-            arr[j+gap] = temp
-        gap = math.floor(gap/3)
+            j = i - gap
+            while j >= 0 and arr[j] > temp:
+                arr[j + gap] = arr[j]
+                j -= gap
+            arr[j + gap] = temp
+        gap = math.floor(gap / 3)
     return arr
 
 
 # 归并排序
 def mergeSort(arr):
     import math
-    if(len(arr)<2):
+    if (len(arr) < 2):
         return arr
-    middle = math.floor(len(arr)/2)
+    middle = math.floor(len(arr) / 2)
     left, right = arr[0:middle], arr[middle:]
     return merge(mergeSort(left), mergeSort(right))
 
+
 # 归并递归处理函数
-def merge(left,right):
+def merge(left, right):
     result = []
     while left and right:
         if left[0] <= right[0]:
@@ -74,28 +99,31 @@ def merge(left,right):
         result.append(right.pop(0));
     return result
 
+
 # 快速排序
 def quickSort(arr, left=None, right=None):
-    left = 0 if not isinstance(left,(int, float)) else left
-    right = len(arr)-1 if not isinstance(right,(int, float)) else right
+    left = 0 if not isinstance(left, (int, float)) else left
+    right = len(arr) - 1 if not isinstance(right, (int, float)) else right
     if left < right:
         partitionIndex = partition(arr, left, right)
-        quickSort(arr, left, partitionIndex-1)
-        quickSort(arr, partitionIndex+1, right)
+        quickSort(arr, left, partitionIndex - 1)
+        quickSort(arr, partitionIndex + 1, right)
     return arr
+
 
 # 快速排序处理基准
 def partition(arr, left, right):
     pivot = left
-    index = pivot+1
+    index = pivot + 1
     i = index
-    while  i <= right:
+    while i <= right:
         if arr[i] < arr[pivot]:
             swap(arr, i, index)
-            index+=1
-        i+=1
-    swap(arr,pivot,index-1)
-    return index-1
+            index += 1
+        i += 1
+    swap(arr, pivot, index - 1)
+    return index - 1
+
 
 # 快速排序交换, 堆排序交换
 def swap(arr, i, j):
@@ -105,12 +133,13 @@ def swap(arr, i, j):
 # 堆排序
 def buildMaxHeap(arr):
     import math
-    for i in range(math.floor(len(arr)/2),-1,-1):
-        heapify(arr,i)
+    for i in range(math.floor(len(arr) / 2), -1, -1):
+        heapify(arr, i)
+
 
 def heapify(arr, i):
-    left = 2*i+1
-    right = 2*i+2
+    left = 2 * i + 1
+    right = 2 * i + 2
     largest = i
     if left < arrLen and arr[left] > arr[largest]:
         largest = left
@@ -120,32 +149,33 @@ def heapify(arr, i):
         swap(arr, i, largest)
         heapify(arr, largest)
 
+
 def heapSort(arr):
     global arrLen
     arrLen = len(arr)
     buildMaxHeap(arr)
-    for i in range(len(arr)-1,0,-1):
-        swap(arr,0,i)
-        arrLen -=1
+    for i in range(len(arr) - 1, 0, -1):
+        swap(arr, 0, i)
+        arrLen -= 1
         heapify(arr, 0)
     return arr
 
 
 # 计数排序
 def countingSort(arr, maxValue):
-    bucketLen = maxValue+1
-    bucket = [0]*bucketLen
-    sortedIndex =0
+    bucketLen = maxValue + 1
+    bucket = [0] * bucketLen
+    sortedIndex = 0
     arrLen = len(arr)
     for i in range(arrLen):
         if not bucket[arr[i]]:
-            bucket[arr[i]]=0
-        bucket[arr[i]]+=1
+            bucket[arr[i]] = 0
+        bucket[arr[i]] += 1
     for j in range(bucketLen):
-        while bucket[j]>0:
+        while bucket[j] > 0:
             arr[sortedIndex] = j
-            sortedIndex+=1
-            bucket[j]-=1
+            sortedIndex += 1
+            bucket[j] -= 1
     return arr
 
 
@@ -154,14 +184,14 @@ def radix(arr):
     digit = 0
     max_digit = 1
     max_value = max(arr)
-    #找出列表中最大的位数
-    while 10**max_digit < max_value:
+    # 找出列表中最大的位数
+    while 10 ** max_digit < max_value:
         max_digit = max_digit + 1
     while digit < max_digit:
         temp = [[] for i in range(10)]
         for i in arr:
-            #求出每一个元素的个、十、百位的值
-            t = int((i/10**digit)%10)
+            # 求出每一个元素的个、十、百位的值
+            t = int((i / 10 ** digit) % 10)
             temp[t].append(i)
         coll = []
         for bucket in temp:
@@ -170,6 +200,7 @@ def radix(arr):
         arr = coll
         digit = digit + 1
     return arr
+
 
 if __name__ == '__main__':
     print('test start')
